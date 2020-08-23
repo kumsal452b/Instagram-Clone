d7bc00d66e71505190e4d816c9725c39288599f8
kumsal67 <70024440+kumsal67@users.noreply.github.com>
2020-08-23 19:37:26 +0300

    Added to nedded component in projects

--- app/src/main/java/com/example/instagram_clone/MainActivity.java
+++ app/src/main/java/com/example/instagram_clone/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.instagram_clone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -15,6 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        username=findViewById(R.id.)
+        username=findViewById(R.id.editTextTextPersonName2);
+        password=findViewById(R.id.editTextTextPassword);
+        btnSgnIn=findViewById(R.id.btn_Sgnin);
+        btnSgUp=findViewById(R.id.btn_Sgnup);
+        btnSgUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+        btnSgnIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
