5810db1dcb1490291d97158fefdc08a4ea028530
kumsal67 <70024440+kumsal67@users.noreply.github.com>
2020-08-23 19:34:04 +0300

    Project Desingned finished

--- app/src/main/java/com/example/instagram_clone/MainActivity.java
+++ app/src/main/java/com/example/instagram_clone/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.instagram_clone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText username;
+    EditText password;
+    Button btnSgnIn,btnSgUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username=findViewById(R.id.)
     }
 }
